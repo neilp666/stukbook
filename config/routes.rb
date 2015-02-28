7ea56cbb9ddb0c13252631ae5158f640b1dc2545
neil patel <neil@myindianweddingapp.com>
2015-02-28 10:54:16 +0000

    Add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+ 
   devise_for :users, controllers: {registrations: 'registrations'}
   
   root 'pages#home'
+
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
