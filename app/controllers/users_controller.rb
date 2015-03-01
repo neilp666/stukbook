026bcb027e816c6e4d34a9b4da9431881325d728
neil patel <neil@myindianweddingapp.com>
2015-03-01 19:20:53 +0000

    Added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 	before_action :set_user, only:[:show]
-  def show
-  end
 
-  private
+def index
+	@users = User.all
+end
 
-  def set_user
-  	@user = User.find_by(username: params[:id])
-  end
+def show
+end
+
+
+private
+
+	def set_user
+	 @user = User.find_by(username: params[:id])
+	end
 end
