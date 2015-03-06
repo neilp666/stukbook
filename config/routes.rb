cde479b9c6aebe8b539ebb3383034afe5b8ee6cd
neil patel <neil@myindianweddingapp.com>
2015-03-06 12:51:27 +0000

    Created Friendship requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :users, only: [:show, :index]
+  resources :friendships, only: [:create, :destroy, :accept] do
+    member do
+      put :accept
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
